38344d5ae1bfb53ec8d5e737a11a8e2fbfd97988
Nikhil Kumar <nikumar1206@gmail.com>
2025-06-03 19:21:33 -0400

    setup with hcp for state management

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,6 +6,16 @@ terraform {
     }
   }
 
+  backend "remote" {
+
+    hostname = "app.terraform.io"
+    organization = "loco-deploy"
+    workspaces {
+      name = "loco"
+    }
+    
+  }
+
   required_version = ">= 1.2.0"
 }
 
