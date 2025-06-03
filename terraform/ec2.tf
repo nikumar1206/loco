0740d868d0de9f5dbd4ed0c2a614d86bf8f31274
Nikhil Kumar <nikumar1206@gmail.com>
2025-06-02 22:24:04 -0400

    removing ec2

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "first-ec2" {
-    ami = "ami-0305634128cf9fde8"
-    instance_type = "t4g.nano"
-}
+# resource "aws_instance" "first-ec2" {
+#     ami = "ami-0305634128cf9fde8"
+#     instance_type = "t4g.nano"
+# }
